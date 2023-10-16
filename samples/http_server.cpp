ed0806f7d7c218768bddf39b16602f9b9ea988b6
shao <vampirictutor@gmail.com>
2023-10-16 20:42:28 +0900

    start point, web server handles '/foo'

--- samples/http_server.cpp
+++ samples/http_server.cpp
@@ -11,6 +11,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string>
 
 #include "EventLoop.h"
 #include "signals.h"
@@ -27,38 +28,13 @@ public:
         :BaseHttpServer( loop, addr, port )
     {
     }
+
+    static const char * decode_method(int req_method );
     
     void generic_handler(struct evhttp_request * req); 
+    
+    void handle_foo(struct evhttp_request * req);
 };
-
-class HandlerFoo
-    :public SimpleHttpRequestHandler
-{
-public:
-     HandlerFoo(SimpleEventLoop  * loop, BaseHttpServer * server)
-         :SimpleHttpRequestHandler(loop,  server, "/foo")
-    {
-    }
-
-    virtual void handler(struct evhttp_request * req)
-    { 
-        struct evbuffer* out_buf = evhttp_request_get_output_buffer(req);
-        if (!out_buf)
-        {
-            LOG_WARN("Failed to  evhttp_request_get_output_buffer(). \n");
-            return;
-        }
-
-        evbuffer_add_printf(out_buf
-                , "<html><body>Yes, I am foo.</body></html>\n");
-
-        evhttp_send_reply(req, HTTP_OK, "", out_buf);
-
-    }
-
-
-};
-
 
 int main(int argc, char **argv)
 {
@@ -73,8 +49,6 @@ int main(int argc, char **argv)
         //1. listen as http server
         SampleHttpServer server(&loop, "0.0.0.0", PORT); 
 
-        HandlerFoo foo(&loop, &server);
- 
         //2. also we handle ctrl-C
         QuitSignalHandler control_c_handler(&loop);
 
@@ -97,9 +71,8 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void  SampleHttpServer::generic_handler(struct evhttp_request * req)
+const char * SampleHttpServer::decode_method(int req_method )
 { 
-    int req_method = evhttp_request_get_command(req);
     const char *cmdtype;
 
 	switch (req_method ) {
@@ -115,13 +88,19 @@ void  SampleHttpServer::generic_handler(struct evhttp_request * req)
 	default: cmdtype = "unknown"; break;
 	}
 
+    return cmdtype;
 
-    std::string uri = evhttp_request_get_uri(req);
+}
 
-    struct evbuffer *buf;
-	buf = evhttp_request_get_input_buffer(req);
 
-	CString s("Received a %s request for %s, req = %p\n"
+void  SampleHttpServer::generic_handler(struct evhttp_request * req)
+{ 
+    int req_method = evhttp_request_get_command(req);
+    const char *cmdtype =   SampleHttpServer::decode_method(req_method);
+
+    const std::string uri = evhttp_request_get_uri(req);
+
+	CString s("Received a %s request for %s , req = %p\n"
             ,  cmdtype
             , uri.c_str()
             , req
@@ -139,7 +118,10 @@ void  SampleHttpServer::generic_handler(struct evhttp_request * req)
 	s+= "post data: <<<\n";
     LOG_DEBUG("%s", s.c_str());
 
-    DynaBuf post_data;
+    DynaBuf post_data; 
+    struct evbuffer *buf;
+	buf = evhttp_request_get_input_buffer(req);
+
     size_t data_len = evbuffer_get_length(buf); 
     post_data.resize( data_len + 1);
     post_data[data_len] = 0;
@@ -149,15 +131,54 @@ void  SampleHttpServer::generic_handler(struct evhttp_request * req)
 	fwrite(& post_data[0], 1, data_len , stderr);    // LOG_XXX 最终写到stderr
 	RAW_LOG("\n>>>\n");
 
-    //////////////////////////////////////////////////////////////
-
+    ////////////////////////////////////////////////////////////// 
     struct evbuffer* out_buf = evhttp_request_get_output_buffer(req);
+    try
+    {
+        if ("/foo" == uri )
+        {
+            this->handle_foo(req);
+        }
+    }
+    catch (std::exception& e)
+    { 
+        LOG_ERROR( "While handle '%s', got '%s'.\n"
+            , uri.c_str()
+            , e.what());
+
+        evbuffer_add_printf(out_buf
+            , "While handle '%s', got '%s'.\n"
+            , uri.c_str()
+            , e.what()
+            );
+
+        evhttp_send_reply(req, HTTP_OK, "", out_buf);
+        
+        return;
+    }
+
     evbuffer_add_printf(out_buf
             , "'%s' is not handled.\n", uri.c_str());
 
     evhttp_send_reply(req, HTTP_NOTFOUND, "", out_buf);
     
     //LOG_WARN("'%s' is not handled.\n", uri.c_str());
+
+}
+
+void SampleHttpServer::handle_foo(struct evhttp_request * req)
+{ 
+    struct evbuffer* out_buf = evhttp_request_get_output_buffer(req);
+    if (!out_buf)
+    {
+        LOG_WARN("Failed to  evhttp_request_get_output_buffer(). \n");
+        return;
+    }
+
+    evbuffer_add_printf(out_buf
+            , "<html><body>Yes, I am foo.</body></html>\n");
+
+    evhttp_send_reply(req, HTTP_OK, "", out_buf);
 
 }
 
